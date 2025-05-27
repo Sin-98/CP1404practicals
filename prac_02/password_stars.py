9d488bc7e5408ba0ad16fc61085c949069f9acff
Sin <sinnather.paing@my.jcu.edu.au>
2025-05-27 12:52:59 +0800

    add docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,14 +1,17 @@
 MINIMUM_LENGTH = 6
 def main():
+    """Get password and print asterisks using functions."""
     password = get_password()
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Print asterisks as long as the password."""
     print("*" * len(password))
 
 
 def get_password():
+    """Get valid password according to minimum length."""
     password = input(f"Enter password of minimum length {MINIMUM_LENGTH}: ")
     while len(password) < MINIMUM_LENGTH:
         password = input(f"Enter password of minimum length {MINIMUM_LENGTH}: ")
