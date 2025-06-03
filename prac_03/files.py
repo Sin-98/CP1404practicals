a278b3d338fec61c241720ab3b89b6b684294cd3
Sin <sinnather.paing@my.jcu.edu.au>
2025-06-03 13:21:46 +0800

    answer question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,9 @@ out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
 
+# 2.
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
+
