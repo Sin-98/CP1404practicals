017b49cdb1378760b1d6cd9dc211c950e0dc2ad5
Sin <sinnather.paing@my.jcu.edu.au>
2025-06-03 12:16:49 +0800

    answered all questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.randint(5, 20))  # line 1 => saw 6, smallest is 5 and largest is 20
+print(random.randrange(3, 10, 2))  # line 2 => saw 3, smallest is 3 and largest is 9
+print(random.uniform(2.5, 5.5))  # line 3 => saw 3.0627455392033274, smallest is 2.5 and largest is 5.5
+print(random.randint(1, 100))
