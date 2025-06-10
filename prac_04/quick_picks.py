f9c7137508f5d05593eab2ca17885a353482fc3a
Sin <sinnather.paing@my.jcu.edu.au>
2025-06-10 14:33:08 +0800

    add validation of number of quick picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,6 +5,9 @@ MINIMUM = 1
 MAXIMUM = 45
 
 number_of_quick_picks = int(input("How many quick picks? "))
+while number_of_quick_picks < 0:
+    print("Invalid input")
+    number_of_quick_picks = int(input("How many quick picks? "))
 for i in range(number_of_quick_picks):
     quick_picks = []
     for j in range(NUMBERS_PER_LINE):
