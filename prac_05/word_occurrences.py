173e477b0aa992fe7a79a752a3b32f1d9fa45fa5
Sin <sinnather.paing@my.jcu.edu.au>
2025-06-17 14:32:35 +0800

    put the word and its count in dictionary by using LBYL approach

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,4 +7,9 @@ Actual:   32 minutes
 word_to_count = {}
 text = input("Enter text: ")
 words = text.split()
+for word in words:
+    count = word_to_count.get(word, 0)
+    word_to_count[word] = count + 1
+
 print(words)
+print(word_to_count)
