77957435cdf208e07c7a3204c87f005340af5988
Sin <sinnather.paing@my.jcu.edu.au>
2025-06-17 14:37:40 +0800

    align the output so the numbers are in one column

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -13,3 +13,5 @@ for word in words:
 words = list(word_to_count.keys())
 words.sort()
 max_length = max(len(word) for word in words)
+for word in words:
+    print(f"{word:{max_length}} : {word_to_count[word]}")
