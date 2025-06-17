0e6636ee16a8edf141959f9d525c53f2bef2a61b
Sin <sinnather.paing@my.jcu.edu.au>
2025-06-17 15:05:42 +0800

    validate email and loop through dictionary and print

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,13 +6,16 @@ Actual:    minutes
 
 def main():
     email_to_name = {}
-    email = input("Enter email: ")
-    name = get_name_from_email(email)
-    checking = input(f"Is your name {name}? (Y/n) ")
-    if checking.upper() != "Y" and checking != "":
-        name = input("Name: ")
-    email_to_name[email] = name
-
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        checking = input(f"Is your name {name}? (Y/n) ")
+        if checking.upper() != "Y" and checking != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 def get_name_from_email(email):
     """Get name from email."""
