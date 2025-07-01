7a2798dd6378f73aac0bb53c0913a24b3e9c7da2
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-01 17:49:16 +0800

    create inti function

---
+++ prac_06/guitar.py
@@ -0,0 +1,13 @@
+"""
+Guitar
+Estimate: 30 minutes
+Actual:    minutes
+"""
+
+class Guitar:
+    """Represents details of a guitar."""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar object."""
+        self.name = name
+        self.year = year
+        self.cost = cost
