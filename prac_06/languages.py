81753614ce9b5db4f6195ed89d6fabbdc46321ec
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-01 17:37:29 +0800

    check if the printing works

---
+++ prac_06/languages.py
@@ -0,0 +1,17 @@
+"""
+Languages
+Estimate: 15 minutes
+Actual:    minutes
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+main()
