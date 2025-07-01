71ccb15466a38542f3a3e4662ff839e024d11c04
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-01 17:25:56 +0800

    create inti function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+"""
+Programming language
+Estimate: 20 minutes
+Actual:    minutes
+"""
+
+class ProgrammiingLanguage:
+    """Represent a programming language."""
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a programming language."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
