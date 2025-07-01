62ca75e573d3bdb65e982a0637cffb1e23c384e7
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-01 18:10:15 +0800

    add get_age method to the class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,6 +3,7 @@ Guitar
 Estimate: 30 minutes
 Actual:    minutes
 """
+from datetime import date
 
 class Guitar:
     """Represents details of a guitar."""
@@ -15,3 +16,8 @@ class Guitar:
     def __str__(self):
         """Return a string representation of the Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the Guitar."""
+        current_year = date.today().year
+        return current_year - self.year
