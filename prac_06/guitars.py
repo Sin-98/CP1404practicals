1ba015faaa30978a346912a96a780ecffbe8354a
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-01 18:41:43 +0800

    print the details of the added guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -16,5 +16,17 @@ def main():
         cost = float(input("Cost: $"))
         guitar_to_add = Guitar(name, year, cost)
         guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added.")
         name = input("Name: ")
 
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+main()
