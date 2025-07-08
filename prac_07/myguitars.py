18ede17c0c9b3ec8323d764217076431a0dda4a2
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-08 15:25:04 +0800

    Get the user to add new guitars and add them into the guitars list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -12,6 +12,9 @@ def main():
     print("\nGuitars sorted by year:")
     display_guitars(guitars)
 
+    new_guitars = get_new_guitars()
+    guitars += new_guitars
+
 def load_guitars(guitars):
     """Read guitars from a file and return a list of Guitar objects."""
     guitars = []
@@ -26,5 +29,18 @@ def display_guitars(guitars):
     """Display a list of Guitar objects."""
     for guitar in guitars:
         print(guitar)
+
+def get_new_guitars():
+    """Get the user to add new guitars and return them as a list."""
+    new_guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        new_guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added.")
+        name = input("Name: ")
+    return new_guitars
 
 main()
