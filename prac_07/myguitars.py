19a68199c36495dc9c7572238881b2f4bc539828
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-08 15:03:50 +0800

    Read guitars from a file and return a list of Guitar objects

---
+++ prac_07/myguitars.py
@@ -0,0 +1,18 @@
+from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+def main():
+    """Read, display, add and save guitars."""
+    guitars = load_guitars(FILENAME)
+
+
+def load_guitars(guitars):
+    """Read guitars from a file and return a list of Guitar objects."""
+    guitars = []
+    with open(FILENAME, "r") as in_file:
+        for line in in_file:
+            parts = line.strip().split(",")
+            name, year, cost = parts[0], int(parts[1]), float(parts[2])
+            guitars.append(Guitar(name, year, cost))
+    return guitars
