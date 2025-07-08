d24aff5d550e562ed3d9a4bea485c6bfcaf14666
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-08 15:12:09 +0800

    Add __lt__ method in the class

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -21,3 +21,6 @@ class Guitar:
     def is_vintage(self):
         """Determine if the Guitar is vintage."""
         return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        return self.year < other.year
