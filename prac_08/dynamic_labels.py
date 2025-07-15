8f2c2371f8021affc4fa459682fcfe2e8186284d
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-15 15:42:06 +0800

    Build the Kivy GUI

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    """Kivy app to create dynamic labels for a list of names."""
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ["Alice", "Bob", "Charlie", "Dana", "Eli"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self_title = 'Dynamic Labels'
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+
+
+DynamicLabelsApp().run()
