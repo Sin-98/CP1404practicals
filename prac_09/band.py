0d9aa2129efd2e77cde0e0468b84b8d667f12a81
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-22 15:27:00 +0800

    Add __str__ method

--- prac_09/band.py
+++ prac_09/band.py
@@ -5,7 +5,10 @@ class Band:
         self.name = name
         self.members = []
 
-
+    def __str__(self):
+        """Return a string representation of the band."""
+        members_str = ", ".join(str(member) for member in self.members)
+        return f"{self.name} ({members_str})"
 
     def add(self, musician):
         """Add a musician to the band."""
