a40d81be90c798ddc287d47ca24d0715a704294d
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-22 14:51:07 +0800

    Add get_fare method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,6 +2,7 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Represent a SilverServiceTaxi."""
+    flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
         """Initialize a SilverServiceTaxi."""
@@ -9,3 +10,6 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
+    def get_fare(self):
+        """Get the fare."""
+        return self.flagfall + super().get_fare()
