96a382ad6e3288ab7b99c514d77ad903a5f5eb98
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-22 15:26:44 +0800

    Add play method

--- prac_09/band.py
+++ prac_09/band.py
@@ -5,6 +5,12 @@ class Band:
         self.name = name
         self.members = []
 
+
+
     def add(self, musician):
         """Add a musician to the band."""
         self.members.append(musician)
+
+    def play(self):
+        """Return a string of each member playing."""
+        return "\n".join(member.play() for member in self.members)
