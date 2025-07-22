4bdfdbe53d8a9ae0f269826da6adebff39dc7a24
Sin <sinnather.paing@my.jcu.edu.au>
2025-07-22 14:54:02 +0800

    Add __str__ method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -10,6 +10,10 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
     def get_fare(self):
         """Get the fare."""
         return self.flagfall + super().get_fare()
